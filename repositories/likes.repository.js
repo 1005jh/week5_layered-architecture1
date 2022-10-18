6ebb67920a6766af244eedd65891daaffbc818d3
jeonghyeon <kjhhy1005@gmail.com>
2022-10-18 21:53:43 +0900

    좋아요기능 repository

--- repositories/likes.repository.js
+++ repositories/likes.repository.js
@@ -1,12 +1,33 @@
-const { Like } = require('../models');
+const { Like, Post } = require("../models");
 
 class LikeRepository {
+  findOneLike = async (userId, postId) => {
+    const findOneLike = await Like.findOne({ where: { userId, postId } });
+    return findOneLike;
+  };
+  upLikePost = async (postId) => {
+    const updateLikePost = await Post.update(
+      { likes: 1 },
+      { where: { postId } }
+    );
+    return updateLikePost;
+  };
+  downLikePost = async (postId) => {
+    const updateLikePost = await Post.update(
+      { likes: -1 },
+      { where: { postId } }
+    );
+    return updateLikePost;
+  };
+  createLike = async (userId, postId) => {
+    const createLike = await Like.create({ userId, postId });
 
-  
-  
+    return createLike;
+  };
+  destroyLike = async (userId, postId) => {
+    const destroyLike = await Like.destroy({ where: { userId, postId } });
+    return destroyLike;
+  };
+}
 
-    
-  
-  }
-  
-  module.exports = LikeRepository;
+module.exports = LikesRepository;
